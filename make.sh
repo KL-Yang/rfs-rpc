0565be7bcc65f6922c28f438a02d728f8c9b62bd
yangkunlun <yangkunlun@gmail.com>
2014-02-26 21:56:58 +0800

    add header file and compile to lib.a

--- make.sh
+++ make.sh
@@ -3,5 +3,6 @@ rpcgen rfs_rpc.x
 #I will test rpcgen -M rfs.x later for thread safe mode
 gcc -ggdb -c rfs_rpc_clnt.c rfs_rpc_xdr.c rfs_rpc_svc.c server.c
 gcc -ggdb -c -W -Wall client.c test.c
-gcc client.o test.o rfs_rpc_clnt.o rfs_rpc_xdr.o -o client -lnsl
+ar cr librfs_simple.a client.o rfs_rpc_clnt.o rfs_rpc_xdr.o
+gcc test.o -o client librfs_simple.a -lnsl
 gcc server.o rfs_rpc_svc.o rfs_rpc_xdr.o -o server -lnsl
